989da3d4d16d59ca52f3f1a5c02068dff482c9da
salma akg <salmaakagou1@gmail.com>
2025-10-17 14:59:27 +0100

    ajout project 'live coding prototype'

---
+++ liveCoding-prototype/app/Services/DeveloperService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class DeveloperService{
+    public function getProfile(){
+        return [
+            'name' => 'Salma Akajou',
+            'role' => 'Développeuse Full Stack',
+            'bio' => "Passionnée par la création d'applications web modernes et bien structurées.",
+            'email' => 'salmaakajou@gmail.com',
+        ];
+    }
+}
